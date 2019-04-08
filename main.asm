14fdd1318db7cd37163ab8d8ec947e0510b3e3f3
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-04-08 15:23:44 +0200

    Initial commit

---
+++ main.asm
@@ -0,0 +1,17 @@
+;
+; Sem3_CAO_Ex2.asm
+;
+; Created: 4/8/2019 2:45:13 PM
+; Author : andre
+;
+
+
+; Replace with your application code
+start:
+    ;inc r16
+
+	
+	LDI R16, 39 ;Load Imediatelly into Register 16 value "39"
+	NOP ; does nothing, but provides a place to put a breakpoint
+
+    rjmp start
